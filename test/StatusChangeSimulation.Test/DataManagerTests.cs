d48fc5bef2994a75d79c4aa241e3a89bff599628
markharrop <mark.harrop@education.gov.uk>
2026-08-28 13:37:55 +0100

    Add a final stage to test to ensure reverting to a previous stage is detected

--- test/StatusChangeSimulation.Test/DataManagerTests.cs
+++ test/StatusChangeSimulation.Test/DataManagerTests.cs
@@ -50,21 +50,22 @@ public class DataManagerTests
     public async Task SimulateDataChange()
     {
         // baseline existing state rows
-        
         await _datamanager.SimulateDataUpdate(SchoolcensusSpring, "8503023", 1, 2, 3, 7);
         await _datamanager.SimulateDataUpdate(SchoolcensusSpring, "8153008", 3, 4, 5, 7);
         await _datamanager.SimulateDataUpdate(SchoolcensusSpring, "8502389", 4, 5, 6, 7);
         
         await _datamanager.ClearDownLedger();
         await _datamanager.UpdateCensusLedger(SchoolcensusSpring, 7);
+        
+        // simulate a change for three schools
         var originalCount = await _datamanager.GetLedgerCount();
         await _datamanager.SimulateDataUpdate(SchoolcensusSpring, "8503023", 10, 11, 12, 8);
         await _datamanager.SimulateDataUpdate(SchoolcensusSpring, "8153008", 11, 12, 13, 8);
         await _datamanager.SimulateDataUpdate(SchoolcensusSpring, "8502389", 12, 13, 14, 8);
-        await _datamanager.UpdateCensusLedger(SchoolcensusSpring, 0);
+        await _datamanager.UpdateCensusLedger(SchoolcensusSpring, 6);
         var updatedCount = await _datamanager.GetLedgerCount();
         Assert.Equal(3, updatedCount - originalCount);
-        await _datamanager.UpdateCensusLedger(SchoolcensusSpring, 0);
+        await _datamanager.UpdateCensusLedger(SchoolcensusSpring, 6);
         updatedCount = await _datamanager.GetLedgerCount();
         Assert.Equal(3, updatedCount - originalCount);
         
@@ -88,5 +89,39 @@ public class DataManagerTests
         Assert.Equal(13, result3.Queries);
         Assert.Equal(14, result3.OkdErrorsQueries);
         Assert.Equal(8, result3.ReturnStatusCode);
+        
+        // set back to original to ensure we are tracking changes, not unique rows
+        
+        await _datamanager.SimulateDataUpdate(SchoolcensusSpring, "8503023", 1, 2, 3, 7);
+        await _datamanager.SimulateDataUpdate(SchoolcensusSpring, "8153008", 3, 4, 5, 7);
+        await _datamanager.SimulateDataUpdate(SchoolcensusSpring, "8502389", 4, 5, 6, 7);
+        
+        await _datamanager.UpdateCensusLedger(SchoolcensusSpring, 5);
+        var finalCount = await _datamanager.GetLedgerCount();
+        Assert.Equal(3, finalCount - updatedCount);
+        // run again to prove it is idempotent
+        finalCount = await _datamanager.GetLedgerCount();
+        Assert.Equal(3, finalCount - updatedCount);
+        
+        var result4 = await _datamanager.GetCensusSummary(SchoolcensusSpring, "8503023");
+        Assert.NotNull(result4);
+        Assert.Equal(1, result4.Errors);
+        Assert.Equal(2, result4.Queries);
+        Assert.Equal(3, result4.OkdErrorsQueries);
+        Assert.Equal(7, result4.ReturnStatusCode);
+        
+        var result5 = await _datamanager.GetCensusSummary(SchoolcensusSpring, "8153008");
+        Assert.NotNull(result5);
+        Assert.Equal(3, result5.Errors);
+        Assert.Equal(4, result5.Queries);
+        Assert.Equal(5, result5.OkdErrorsQueries);
+        Assert.Equal(7, result5.ReturnStatusCode);
+        
+        var result6 = await _datamanager.GetCensusSummary(SchoolcensusSpring, "8502389");
+        Assert.NotNull(result6);
+        Assert.Equal(4, result6.Errors);
+        Assert.Equal(5, result6.Queries);
+        Assert.Equal(6, result6.OkdErrorsQueries);
+        Assert.Equal(7, result6.ReturnStatusCode);        
     }
 }
