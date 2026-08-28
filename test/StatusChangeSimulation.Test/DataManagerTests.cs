af8524f31b65dc37445970e100af4a06f9339aba
markharrop <mark.harrop@education.gov.uk>
2026-08-28 10:04:00 +0100

    Add basic end to end test

--- test/StatusChangeSimulation.Test/DataManagerTests.cs
+++ test/StatusChangeSimulation.Test/DataManagerTests.cs
@@ -45,4 +45,27 @@ public class DataManagerTests
         var results = await _datamanager.GetCensusSummary(SchoolcensusSpring, "3712195");
         Assert.NotNull(results);
     }
+
+    [Fact]
+    public async Task SimulateDataChange()
+    {
+        // baseline existing state rows
+        
+        await _datamanager.SimulateDataUpdate(SchoolcensusSpring, "8503023", 1, 2, 3, 7);
+        await _datamanager.SimulateDataUpdate(SchoolcensusSpring, "8153008", 3, 4, 5, 7);
+        await _datamanager.SimulateDataUpdate(SchoolcensusSpring, "8502389", 4, 5, 6, 7);
+        
+        await _datamanager.ClearDownLedger();
+        await _datamanager.UpdateCensusLedger(SchoolcensusSpring, 7);
+        var originalCount = await _datamanager.GetLedgerCount();
+        await _datamanager.SimulateDataUpdate(SchoolcensusSpring, "8503023", 10, 11, 12, 8);
+        await _datamanager.SimulateDataUpdate(SchoolcensusSpring, "8153008", 11, 12, 13, 8);
+        await _datamanager.SimulateDataUpdate(SchoolcensusSpring, "8502389", 12, 13, 14, 8);
+        await _datamanager.UpdateCensusLedger(SchoolcensusSpring, 0);
+        var updatedCount = await _datamanager.GetLedgerCount();
+        Assert.Equal(3, updatedCount - originalCount);
+        await _datamanager.UpdateCensusLedger(SchoolcensusSpring, 0);
+        updatedCount = await _datamanager.GetLedgerCount();
+        Assert.Equal(3, updatedCount - originalCount);
+    }
 }
